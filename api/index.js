f3cf311771432500b4874ef3453c281b70cb9293
Tonny Dong <tonny.dong@autodesk.com>
2019-06-28 17:43:28 +0800

    Add socket support

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const moment = require('moment');
+const socket = require('socket.io');
 
 const app = express();
+
+const io = socket(app);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', '[DEFAULT]' + msg);
+  });
+});
 
 app.use(helmet());
 
